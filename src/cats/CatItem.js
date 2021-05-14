ecb3e2f7e0ac44142dbb59e642e9621d1a8819e5
keven-ruiz <kevenruizh@gmail.com>
2021-05-14 14:17:17 -0700

    we dont need no pictures

--- src/cats/CatItem.js
+++ src/cats/CatItem.js
@@ -11,7 +11,7 @@ export default class CatItem extends Component {
       <li className="CatItem">
         <Link to={`/cats/${cat.id}`}>
           <h2>{cat.name}</h2>
-          <img src={cat.url} alt={cat.name} />
+
           <p>Lives: {cat.lives}</p>
         </Link>
 

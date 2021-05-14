4fbf69c77ad3e877e67e5ef0296ae7297ecd8235
keven-ruiz <kevenruizh@gmail.com>
2021-05-14 14:53:56 -0700

    adding the css format, and styling the detail page

--- src/cat/CatDetailPage.js
+++ src/cat/CatDetailPage.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { getCat } from '../utils/cats-api';
 
 export default class CatDetailPage extends Component {
+  state = {
+    cat: null
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    try {
+      const cat = await getCat(match.params.id);
+      this.setState({ cat: cat });
+    }
+    catch (err) {
+      console.log(err.message);
+    }
+    finally {
+      this.setState({});
+    }
+  }
+
   render() {
+    const { cat } = this.state;
+
+    if (!cat) return null;
+
+
     return (
-      <div>
+      <div className="CatDetailPage">
+
+        <h2>{cat.name}</h2>
+
+        <p>Introduced in {cat.year}</p>
+        <p>Has {cat.lives} {cat.lives === 1 ? 'life' : 'lives'}</p>
+        {cat.isSidekick && 'this cat is a sidekick'}
 
       </div>
     );
